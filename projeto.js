303672d1059aa468e8250d8731a559098277444c
Mavihh2 <vmariah297@gmail.com>
2026-06-21 20:45:01 -0300

    Enviando apenas os arquivos do Projeto Prático

---
+++ projeto.js
@@ -0,0 +1,36 @@
+const form = document.getElementById("formContato");
+
+form.addEventListener("submit", function(event){
+
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const mensagem = document.getElementById("mensagem").value;
+
+    if(nome === "" || email === "" || mensagem === ""){
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    const emailValido =
+    /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if(!emailValido.test(email)){
+        alert("Digite um e-mail válido!");
+        return;
+    }
+
+    alert("Mensagem enviada com sucesso!");
+
+    form.reset();
+});
+
+const temaBtn =
+document.getElementById("temaBtn");
+
+temaBtn.addEventListener("click", () => {
+
+    document.body.classList.toggle("light");
+
+});
